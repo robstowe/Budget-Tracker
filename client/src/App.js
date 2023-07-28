63da403c837d61d88c95635acdfec7bd0d7a6d28
Suraj <snair306@gmail.com>
2023-07-28 10:11:14 -0500

    first route working

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" href="/">
+      <a href="/test">Test</a>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Test from './Test';
+import MyNavBar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <MyNavBar />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/test" element={<Test />} />
+    </Routes>
+    
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
