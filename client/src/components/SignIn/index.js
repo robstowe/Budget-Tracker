fdbf3d20605fdaedffe425c4f3f2d02e71f58644
Alex Colin <acolin3196@gmail.com>
2023-07-28 11:40:07 -0500

    changes

--- client/src/components/SignIn/index.js
+++ client/src/components/SignIn/index.js
@@ -1,4 +1,3 @@
-// import * as React from 'react';
 import React, { useState } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
@@ -17,7 +16,8 @@ import { useMutation } from '@apollo/client';
 import { LOGIN_USER, ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
-
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme(); // Replace 'path/to/your/theme' with the actual path to your theme file
 
 function Copyright(props) {
   return (
@@ -32,13 +32,10 @@ function Copyright(props) {
   );
 }
 
-// TODO remove, this demo shouldn't need to reset the theme.
-
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
   const [login, { error, data }] = useMutation(LOGIN_USER);
 
-  // update state based on form input changes
   const handleChange = (event) => {
     const { name, value } = event.target;
 
@@ -48,7 +45,6 @@ const Login = (props) => {
     });
   };
 
-  // submit form
   const handleFormSubmit = async (event) => {
     event.preventDefault();
     console.log(formState);
@@ -62,7 +58,6 @@ const Login = (props) => {
       console.error(e);
     }
 
-    // clear form values
     setFormState({
       email: '',
       password: '',
@@ -119,6 +114,229 @@ const Login = (props) => {
     </main>
   );
 };
+
+function ThemeSection(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Your submit logic here
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+}
+
+export default Login;
+
+
+
+
+
+
+// import * as React from 'react';
+// import React, { useState } from 'react';
+// import Avatar from '@mui/material/Avatar';
+// import Button from '@mui/material/Button';
+// import CssBaseline from '@mui/material/CssBaseline';
+// import TextField from '@mui/material/TextField';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import Checkbox from '@mui/material/Checkbox';
+// import Link from '@mui/material/Link';
+// import Paper from '@mui/material/Paper';
+// import Box from '@mui/material/Box';
+// import Grid from '@mui/material/Grid';
+// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+// import Typography from '@mui/material/Typography';
+// import { createTheme, ThemeProvider } from '@mui/material/styles';
+// import { useMutation } from '@apollo/client';
+// import { LOGIN_USER, ADD_USER } from '../utils/mutations';
+// import Auth from '../utils/auth';
+
+
+
+// function Copyright(props) {
+//   return (
+//     <Typography variant="body2" color="text.secondary" align="center" {...props}>
+//       {'Copyright © '}
+//       <Link color="inherit" href="https://mui.com/">
+//         Your Website
+//       </Link>{' '}
+//       {new Date().getFullYear()}
+//       {'.'}
+//     </Typography>
+//   );
+// }
+
+
+
+// const Login = (props) => {
+//   const [formState, setFormState] = useState({ email: '', password: '' });
+//   const [login, { error, data }] = useMutation(LOGIN_USER);
+
+//   // update state based on form input changes
+//   const handleChange = (event) => {
+//     const { name, value } = event.target;
+
+//     setFormState({
+//       ...formState,
+//       [name]: value,
+//     });
+//   };
+
+//   // submit form
+//   const handleFormSubmit = async (event) => {
+//     event.preventDefault();
+//     console.log(formState);
+//     try {
+//       const { data } = await login({
+//         variables: { ...formState },
+//       });
+
+//       Auth.login(data.login.token);
+//     } catch (e) {
+//       console.error(e);
+//     }
+
+//     // clear form values
+//     setFormState({
+//       email: '',
+//       password: '',
+//     });
+//   };
+
+//   return (
+//     <main className="flex-row justify-center mb-4">
+//       <div className="col-12 col-lg-10">
+//         <div className="card">
+//           <h4 className="card-header bg-dark text-light p-2">Login</h4>
+//           <div className="card-body">
+//             {data ? (
+//               <p>
+//                 Success! You may now head{' '}
+//                 <Link to="/">back to the homepage.</Link>
+//               </p>
+//             ) : (
+//               <form onSubmit={handleFormSubmit}>
+//                 <input
+//                   className="form-input"
+//                   placeholder="Your email"
+//                   name="email"
+//                   type="email"
+//                   value={formState.email}
+//                   onChange={handleChange}
+//                 />
+//                 <input
+//                   className="form-input"
+//                   placeholder="******"
+//                   name="password"
+//                   type="password"
+//                   value={formState.password}
+//                   onChange={handleChange}
+//                 />
+//                 <button
+//                   className="btn btn-block btn-primary"
+//                   style={{ cursor: 'pointer' }}
+//                   type="submit"
+//                 >
+//                   Submit
+//                 </button>
+//               </form>
+//             )}
+
+//             {error && (
+//               <div className="my-3 p-3 bg-danger text-white">
+//                 {error.message}
+//               </div>
+//             )}
+//           </div>
+//         </div>
+//       </div>
+//     </main>
+//   );
+// };
 
 
 
@@ -211,4 +429,4 @@ const Login = (props) => {
 //   );
 // };
 
-export default Login;
+// export default Login;
