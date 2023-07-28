f507e282278e88da048ea66e12bd6479ea17727e
robstowe <robstowe94@gmail.com>
2023-07-28 11:38:15 -0700

    things are doing thing things

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,61 @@
 import React from 'react'
 import {
-    ApolloClient,
-    InMemoryCache,
-    ApolloProvider,
-    createHttpLink,
-  } from '@apollo/client';
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignIn from './components/SignIn/';
 import SignUp from './components/SignUp'
+import Dashboard from './components/Dashboard'
 
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const httpLink = createHttpLink({
-    uri: '/graphql',
-  });
-  
-  // Construct request middleware that will attach the JWT token to every request as an `authorization` header
-  const authLink = setContext((_, { headers }) => {
-    // get the authentication token from local storage if it exists
-    const token = localStorage.getItem('id_token');
-    // return the headers to the context so httpLink can read them
-    return {
-      headers: {
-        ...headers,
-        authorization: token ? `Bearer ${token}` : '',
-      },
-    };
-  });
-  
-  const client = new ApolloClient({
-    // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-  });
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
-    return (
-        <ApolloProvider client={client}>
-                <Router>
+  return (
+    <ApolloProvider client={client}>
+      <Router>
 
-                <Routes>
-                    <Route
-                        path="/"
-                        element={<SignIn />}/>
-                    <Route
-                        path="/sign-up"
-                        element={<SignUp />}/>    
-                </Routes>
+        <Routes>
+          <Route
+            path="/sign-in"
+            element={<SignIn />} />
+          <Route
+            path="/"
+            element={<Dashboard />} />
+          <Route
+            path="/sign-up"
+            element={<SignUp />} />
+        </Routes>
 
 
-                </Router>
-                </ApolloProvider>
-    );
+      </Router>
+    </ApolloProvider>
+  );
 };
--- client/src/components/utils/mutations.js
+++ client/src/components/utils/mutations.js
@@ -6,19 +6,18 @@ export const LOGIN_USER = gql`
       token
       user {
         _id
-        username
+        
       }
     }
   }
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
+  mutation addUser($email: String!, $password: String!) {
+    addUser(email: $email, password: $password) {
       token
       user {
         _id
-        username
       }
     }
   }
