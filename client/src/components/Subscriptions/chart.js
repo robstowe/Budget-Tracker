a8b4a750cf6d28da93fb47d81e82b99a19ad1daa
Suraj <snair306@gmail.com>
2023-08-02 11:57:37 -0500

    get data on charts

--- client/src/components/Subscriptions/chart.js
+++ client/src/components/Subscriptions/chart.js
@@ -1,14 +1,35 @@
 import * as React from 'react';
 import { BarChart } from '@mui/x-charts/BarChart';
 
+
 export default function SubBars({ subscriptions }) {
   console.log('subscriptions', subscriptions);
   
-  
+  const sortedSubs = subscriptions.sort((a, b) => b.amount - a.amount);
+  console.log('sortedSubs', sortedSubs);
+
+  const sortedSubsNames = sortedSubs.map((sub) => sub.subscription);
+  console.log('sortedSubsNames', sortedSubsNames);
+
+  const sortedSubs2 = sortedSubs.slice(0, 3);
+  console.log('sortedSubs2', sortedSubs2);
+
+  const sortedSubsNames2 = sortedSubs2.slice(0, 3).map((sub) => sub.subscription);
+  console.log('sortedSubsNames2', sortedSubsNames2);
+
+  const sortedSubsByAmount = sortedSubs2.map((sub) => sub.amount);
+  console.log('sortedSubsByAmount', sortedSubsByAmount);
+
+  const dataPoints = sortedSubsByAmount.map((amount) => [amount]);
+  console.log('dataPoints', dataPoints);
+
+  const dataPoints2 = sortedSubsNames2.map((name) => [name]);
+  console.log('dataPoints2', dataPoints2);
+
   return (
     <BarChart
-      xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]}
-      series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
+      xAxis={[{ scaleType: 'band', data: dataPoints2 }]}
+      series={[{ data: dataPoints }]}
       width={500}
       height={300}
     />
--- client/src/components/Utilities/chart.js
+++ client/src/components/Utilities/chart.js
@@ -3,10 +3,32 @@ import { BarChart } from '@mui/x-charts/BarChart';
 
 export default function UtilBars({ utilities}) {
   console.log('utilities', utilities);
+
+  const sortedUtilities = utilities.sort((a, b) => b.amount - a.amount);
+  console.log('sortedUtilities', sortedUtilities);
+
+  const sortedUtilitiesNames = sortedUtilities.map((utility) => utility.utility);
+  console.log('sortedUtilitiesNames', sortedUtilitiesNames);
+
+  const sortedUtilities2 = sortedUtilities.slice(0, 3);
+  console.log('sortedUtilities2', sortedUtilities2);
+
+  const sortedUtilitiesNames2 = sortedUtilities2.slice(0, 3).map((utility) => utility.utility);
+  console.log('sortedUtilitiesNames2', sortedUtilitiesNames2);
+
+  const sortedUtilitiesByAmount = sortedUtilities2.map((utility) => utility.amount);
+  console.log('sortedUtilitiesByAmount', sortedUtilitiesByAmount);
+
+  const dataPoints = sortedUtilitiesByAmount.map((amount) => [amount]);
+  console.log('dataPoints', dataPoints);
+
+  const dataPoints2 = sortedUtilitiesNames2.map((name) => [name]);
+  console.log('dataPoints2', dataPoints2);
+  
   return (
     <BarChart
-      xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]}
-      series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
+      xAxis={[{ scaleType: 'band', data: dataPoints2 }]}
+      series={[{ data: dataPoints }]}
       width={500}
       height={300}
     />
