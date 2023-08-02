8e746a90c76b677f406edab424d5f41512dafb34
Alex Colin <acolin3196@gmail.com>
2023-08-02 09:59:55 -0500

    no real changes

--- client/src/components/Subscriptions/modal.js
+++ client/src/components/Subscriptions/modal.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import './style.css';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import { ADD_SUB } from '../../utils/mutations';
